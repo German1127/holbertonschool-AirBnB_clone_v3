f5b1253074e6058bc38c1d4f496b5666bc77c4ba
German1127 <kleverk2023@gmail.com>
2024-04-17 19:17:02 -0700

    task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """
-start a flask app
+Runs the app.py file
 """
 
-from api.v1.views import app_views
-from os import getenv
-from models import storage
+
 from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def app_teardown():
-    """method that calls storage.close"""
+def teardown_appcontext(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def handle_error(e):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
-
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-
-    app.run(host, port, threaded=True)
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
